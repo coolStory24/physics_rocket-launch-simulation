1ed20badc461237c3b256f493f41c1b933b63697
Иван Чай <138519172+iv4n-t3a@users.noreply.github.com>
2024-09-18 01:35:56 +0300

    Add events

---
+++ simulator/events.py
@@ -0,0 +1,38 @@
+class EventHandler:
+    subscribers = []
+
+    @staticmethod
+    def handle_event(event):
+        for subscriber in EventHandler.subscribers:
+            subscriber.handle_event(event)
+
+    @staticmethod
+    def subscribe(obj):
+        EventHandler.subscribers.append(obj)
+
+
+class Event:
+    def __init__(self, time: float):
+        self.time = time
+
+    def to_string_preffix(self):
+        seconds = int(self.time % 60)
+        minutes = int(self.time / 60 % 60)
+        hours = int(self.time / 3600 % 24)
+        days = int(self.time / 3600 / 24)
+        return f"[{days}d {hours}h {minutes}m {seconds}s]: "
+
+    def to_string(self):
+        return "Event with unimplemented to_string()"
+
+
+class CollisionEvent(Event):
+    def __init__(self, time, planet, rocket, collision_angle, finite_speed):
+        super().__init__(time)
+        self.planet = planet
+        self.rocket = rocket
+        self.collision_angle = collision_angle
+        self.finite_speed = finite_speed
+
+    def to_string(self):
+        return self.to_string_preffix() + f"{self.rocket.name} has fallen on {self.planet.name} at {self.collision_angle:.3f} with speed {self.finite_speed:.3f} m/s"
