0910e69c5cbae577cec855a6f9ed38af88018adb
Иван Чай <138519172+iv4n-t3a@users.noreply.github.com>
2024-09-17 11:44:35 +0300

    Add moon rotation

--- simulator/main.py
+++ simulator/main.py
@@ -9,8 +9,8 @@ from simulation import Simulation
 
 if __name__ == '__main__':
     earth = Planet(5.972E24, Point((0, 0)), Vector((0, 0)), 6371E3, math.pi / 12 / 60 / 60)
-    moon = Planet(7.346E22, Point((384E6, 0)), Vector((0.0, 1.022E3)), 1737E3, 0)
-    rocket = BaseRocket(100, Point((7.371E6, 0)), Vector((0.0, 4.35E3)))
+    moon = Planet(7.346E22, Point((384E6, 0)), Vector((0.0, 1.022E3)), 1737E3, math.pi / 12 / 60 / 60 / 27.5)
+    rocket = BaseRocket(100, Point((7.371E6, 0)), Vector((0.0, 7.35E3)))
 
     earth_sprite = SimPlanetaryObject(earth, pygame.Color("deepskyblue"))
     moon_sprite = SimPlanetaryObject(moon, pygame.Color("white"))
